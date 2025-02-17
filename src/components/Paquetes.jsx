5781d37d4a17d3c678885bbc1f61a039c93983dd
Valentín <valentin.lapalma25@gmail.com>
2025-02-17 15:48:03 -0300

    cambio en fetch de la url

--- src/components/Paquetes.jsx
+++ src/components/Paquetes.jsx
@@ -12,7 +12,7 @@ function Paquetes() {
       setError(null);
 
       try {
-        const response = await fetch("https://sistema-snowy.vercel.app/api/products");  
+        const response = await fetch("https://sistema-snowy.vercel.app");  
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`); 
         }

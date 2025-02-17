8ab2d434a13a9369fc9e785b24646e2639eefc58
Valentín <valentin.lapalma25@gmail.com>
2025-02-17 15:05:39 -0300

    nuevamente detalles

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const app = express();
 require("dotenv").config();
 
-const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 
 app.use("/api", routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const app = require("./app")
+// const app = require("./app")
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en puerto ${PORT}`);
+// });
 
-module.exports = app
+// module.exports = app
